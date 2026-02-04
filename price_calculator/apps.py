835f22a81a72bbab41aae890aaed918b95dc2609
Neethu Antony <neethu7654@gmail.com>
2026-02-04 10:27:19 +0530

    Initial commit

---
+++ price_calculator/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PrizeCalculatorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'prize_calculator'
---
+++ price_calculator/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def price_calculator(request):
+    return HttpResponse("Welcome to the House Price Calculator!")
+# Create your views here.
