47a2cf262c4a112a20ba98415eb45220717c057e
Neethu Antony <neethu7654@gmail.com>
2026-02-04 16:58:11 +0530

    change 'prize_calculator' to 'price_calculator'

--- price_calculator/apps.py
+++ price_calculator/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PrizeCalculatorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'prize_calculator'
+    name = 'price_calculator'
