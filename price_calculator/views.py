5440c026609820e49d4eed1d17b6bab3c6b2820d
Neethu Antony <neethu7654@gmail.com>
2026-02-04 17:04:33 +0530

    load the trained model and teach how to predict

--- price_calculator/views.py
+++ price_calculator/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+import joblib
+import pandas as pd
+import os
 
-def price_calculator(request):
-    return HttpResponse("Welcome to the House Price Calculator!")
-# Create your views here.
+# Load trained model
+MODEL_PATH = os.path.join(settings.BASE_DIR.parent, 'ml_model', 'house_price_model.pkl')
+
+model = joblib.load(MODEL_PATH)
+
+def predict_price(request):
+    prediction = None
+
+    if request.method == "POST":
+        data = {
+            "Area": float(request.POST["area"]),
+            "Bedrooms": int(request.POST["bedrooms"]),
+            "Bathrooms": int(request.POST["bathrooms"]),
+            "Floors": int(request.POST["floors"]),
+            "Condition": request.POST["condition"],
+            "Garage": request.POST["garage"],
+            "Location": request.POST["location"],
+            "House_Age": int(request.POST["house_age"]),
+        }
+
+        input_df = pd.DataFrame([data])
+        prediction = model.predict(input_df)[0]
+
+    return render(request, "index.html", {"prediction": prediction})
